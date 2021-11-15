a6e396f5b4352e9302e5b63dd0ffd86591b63f7d
salahuddinemon <emonthex@gmail.com>
2021-11-15 21:12:10 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,146 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
+
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xfwoh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db('sunglass_db');
+        const sunglassCollection = database.collection('sunglass');
+        const ordersCollection = database.collection('orders');
+        const usersCollection = database.collection('users');
+        const reviewsCollection = database.collection('reviews');
+
+        //Get API
+        app.get('/products', async (req, res) => {
+            const cursor = sunglassCollection.find({});
+            const products = await cursor.toArray();
+            res.send(products);
+        })
+
+        //Get All orders
+        app.get('/orders', async (req, res) => {
+            const cursor = ordersCollection.find({});
+            const order = await cursor.toArray();
+            res.send(order);
+        })
+        //Get All reviews
+        app.get('/reviews', async (req, res) => {
+            const cursor = reviewsCollection.find({});
+            const review = await cursor.toArray();
+            res.send(review);
+        })
+
+        // //Get Single product
+        app.get('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const product = await sunglassCollection.findOne(query);
+            res.json(product);
+        })
+
+        //Get Order from API
+        app.get('/custormerorders', async (req, res) => {
+            const email = req.query.email;
+            const query = { customerEmail: email }
+            const cursor = ordersCollection.find(query);
+            const order = await cursor.toArray();
+            res.json(order);
+        })
+
+        //Add Orders API
+        app.post('/orders', async (req, res) => {
+            const order = req.body;
+            const result = await ordersCollection.insertOne(order);
+            res.json(result);
+            console.log(result);
+        })
+
+        //Post API
+        app.post('/products', async (req, res) => {
+            const products = req.body;
+            console.log('hit the post api', products);
+            const result = await sunglassCollection.insertOne(products);
+            console.log(result);
+            res.json(result);
+        })
+
+        //Review API
+        app.post('/reviews', async (req, res) => {
+            const review = req.body;
+            console.log('hit the post api', review);
+            const result = await reviewsCollection.insertOne(review);
+            console.log(result);
+            res.json(result);
+        })
+
+
+        //Delete API
+        app.delete('/orders/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await ordersCollection.deleteOne(query);
+            res.json(result);
+
+        })
+        //Delete API
+        app.delete('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await sunglassCollection.deleteOne(query);
+            res.json(result);
+
+        })
+        //User API
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
+            console.log(result);
+            res.json(result);
+        })
+
+        app.put('/users', async (req, res) => {
+            const user = req.body;
+            const filter = { email: user.email };
+            const options = { upsert: true };
+            const updateDoc = { $set: user };
+            const result = await usersCollection.updateOne(filter, updateDoc, options);
+            res.json(result);
+        })
+
+        app.put('/users/admin', async (req, res) => {
+            const user = req.body;
+            console.log('put', user);
+            const filter = { email: user.email };
+            const updateDoc = { $set: { role: 'admin' } };
+            const result = await usersCollection.updateOne(filter, updateDoc);
+            res.json(result);
+        })
+    }
+    finally {
+        // await client.addListener();
+    }
+}
+run().catch(console.dir);
+
+
+
+app.get('/', (req, res) => {
+    res.send('Hello Sunglass Ecommerce!')
+})
+
+app.listen(port, () => {
+    console.log('Server Running at:', port);
+})
